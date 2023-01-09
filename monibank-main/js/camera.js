f2f77d767b77719530e2d6f6b5c8c2b45d3f74d7
Gustavo Oki <gustavooki.dev@gmail.com>
2023-01-09 16:46:45 -0300

    mod html e add js

---
+++ monibank-main/js/camera.js
@@ -0,0 +1,4 @@
+const botaoIniciarCamera = document.querySelector("[data-video-botao]");
+const campoCamera = document.querySelector("[data-camera]");
+const video = document.querySelector("[data-video]");
+
